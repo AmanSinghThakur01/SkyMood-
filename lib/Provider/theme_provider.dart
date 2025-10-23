f0f4a88a732237777c314c8cb3eb13d04b050a14
AmanSinghThakur01 <thakuramansinghh01@gmail.com>
2025-10-23 23:55:06 +0530

    code updated for provider

---
+++ lib/Provider/theme_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+// ✅ ThemeNotifier class
+class ThemeNotifier extends ChangeNotifier {
+  ThemeMode _themeMode = ThemeMode.light;
+
+  ThemeMode get themeMode => _themeMode;
+
+  void toggleTheme() {
+    _themeMode =
+    _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    notifyListeners();
+  }
+}
